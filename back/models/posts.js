2799b74239dd4423b15d52fb18cedbf45909f8c8
fearlessdeveloper <1dilumn0@gmail.com>
2022-10-19 18:03:06 +0900

    백엔드 추가

--- back/models/posts.js
+++ back/models/posts.js
@@ -4,6 +4,11 @@ const Sequelize = require("sequelize");
 module.exports = class Post extends Sequelize.Model{
     static init(sequelize){
         return super.init({
+            title:{
+                type:Sequelize.STRING(1000),
+                allowNull:false
+            }
+            ,
             content:{
                 type:Sequelize.STRING(1000),
                 allowNull:false,
--- back/routes/search.js
+++ back/routes/search.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {Board} = require("../models");
+const {Board, User} = require("../models");
 const { QueryTypes } = require('sequelize');
 const { sequelize } = require("../models");
 
@@ -21,12 +21,36 @@ router.get("/hit",async(req,res,next)=>{
     try{
         const query = `select boards.name, COUNT(posts.id) as COUNT from boards inner join posts group by boards.name ORDER BY COUNT DESC LIMIT 10`;
         const data = await sequelize.query(query,{type:QueryTypes.SELECT});
-        res.send(data);
+        res.send({code:200,list:data});
     }
     catch(err){
         next(err);
     }
 
+});
+router.get("/top4",async(req,res,next)=>{
+    try{
+        const query = `select boards.name,boards.id, COUNT(posts.id) as COUNT from boards inner join posts group by boards.name ORDER BY COUNT DESC LIMIT 4`;
+        const data = await sequelize.query(query,{type:QueryTypes.SELECT});
+        for (let i=0; i< data.length; i++){
+            const query2 = `select posts.createdAt, posts.userId, posts.clicked, posts.title, posts.id from posts where boardId ="${data[i].id} LIMIT 10"`;
+            const data2 = await sequelize.query(query2,{type:QueryTypes.SELECT});
+            for (let j=0 ;j <data2.length; j++){
+            
+                const temp = await User.findOne({where:{id:data2[j].userId}});
+                data2[j].nickName = temp.nickName;
+            }
+            data[i].posts=[];
+            for (let j=0 ; j<data2.length;j++){
+                data[i].posts.push(data2[j]);
+            }
+        }
+        res.send({list:data,code:200});
+
+    }
+    catch(err){
+        next(err);
+    }
 });
 
 module.exports = router;
