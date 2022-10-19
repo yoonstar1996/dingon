407b7723c2747e13a36b43763b5a6642901e7aa5
jeongwoo <mbh219@naver.com>
2022-10-19 17:06:24 +0900

    d

--- front/src/App.js
+++ front/src/App.js
@@ -1,27 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState,useEffect} from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
 import axios from "axios";
-const Test = (props)=>{
-  const [value,setValue] = useState("");
-  useEffect(async()=>{
-      const data = await axios("http://localhost:8050");
-      setValue(data.data.code);
-      return ()=>{
-        return ;
-      }
-  },[]);
-  return(
-    <>
-      {value}
-    </>
+import Search from "./Search";
 
-  );
-
+const Test = (props) => {
+  const [value, setValue] = useState("");
+  useEffect(async () => {
+    const data = await axios("http://localhost:8050");
+    setValue(data.data.code);
+    return () => {
+      return;
+    };
+  }, []);
+  return <>{value}</>;
 };
 function App() {
   return (
-      <Test></Test> 
+    <div>
+      <Test></Test>
+      <Search></Search>
+    </div>
   );
 }
 
