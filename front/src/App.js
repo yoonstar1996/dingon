a8e51b8c860a3d168aea8255187229627b78b0ca
CHOIALTH <dnjswns4545@gmail.com>
2022-10-19 21:23:05 +0900

    mainBoard1

--- front/src/App.js
+++ front/src/App.js
@@ -1,27 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState,useEffect} from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
 import axios from "axios";
-const Test = (props)=>{
-  const [value,setValue] = useState("");
-  useEffect(async()=>{
-      const data = await axios("http://localhost:8050");
-      setValue(data.data.code);
-      return ()=>{
-        return ;
-      }
-  },[]);
-  return(
-    <>
-      {value}
-    </>
-
-  );
-
+import MainBoards from "./MainBoards";
+const Test = (props) => {
+  const [value, setValue] = useState("");
+  useEffect(async () => {
+    const data = await axios("http://localhost:8050");
+    setValue(data.data.code);
+    return () => {
+      return;
+    };
+  }, []);
+  return <>{value}</>;
 };
 function App() {
   return (
-      <Test></Test> 
+    <>
+      <MainBoards title="베스트 게시판 : "></MainBoards>
+      <MainBoards title="베스트 게시판 : "></MainBoards>
+      <MainBoards title="베스트 게시판 : "></MainBoards>
+      <MainBoards title="베스트 게시판 : "></MainBoards>
+    </>
   );
 }
 
