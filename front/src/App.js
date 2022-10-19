aa5b4f4f3dc1ae74e1297afc53c0e1ab025a439b
enter-the-code <ssoonngg53@naver.com>
2022-10-19 16:43:19 +0900

    assign

--- front/src/App.js
+++ front/src/App.js
@@ -2,18 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import {useState,useEffect} from "react";
 import axios from "axios";
+import Main from './component/Main';
 const Test = (props)=>{
   const [value,setValue] = useState("");
-  useEffect(async()=>{
-      const data = await axios("http://localhost:8050");
-      setValue(data.data.code);
-      return ()=>{
-        return ;
-      }
-  },[]);
+  // useEffect(async()=>{
+  //     const data = await axios("http://localhost:8050");
+  //     setValue(data.data.code);
+  //     return ()=>{
+  //       return ;
+  //     }
+  // },[]);
   return(
     <>
-      {value}
+      <Main/>
     </>
 
   );
