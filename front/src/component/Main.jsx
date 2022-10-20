98aa0065213918f64ea95a899dfad41168b89f52
enter-the-code <ssoonngg53@naver.com>
2022-10-20 13:37:44 +0900

    틀 수정

--- front/src/component/Main.jsx
+++ front/src/component/Main.jsx
@@ -7,18 +7,15 @@ import "../css/AssignModal.css"
 function Main() {
 
 
-
     return (
-        <>
-                <Routes>
-                    <Route path="/" element={{/*원준*/}}></Route>
-                    <Route path="/게시글" element={{/*기영*/}}></Route>
-                    <Route path="/게시글수정" element={{/*기영*/}}></Route>
-                    <Route path="/gallery/:name" element={{/*경민*/}}></Route>
-                    <Route path="/gallery/made" element={{/*정우*/}}></Route>
-                    <Route path="/*" element={{/*원준*/}}></Route>
-                </Routes>
-        </>
+            <Routes>
+                <Route path="/" element={<div>아내가만든 컴포넌트</div>}></Route>
+                <Route path="/post" element={{/*기영*/ }}></Route>
+                <Route path="/postupdate" element={{/*기영*/ }}></Route>
+                <Route path="/gallery/:name" element={{/*경민*/ }}></Route>
+                <Route path="/gallery/made" element={{/*정우*/ }}></Route>
+                <Route path="/*" element={{/*원준*/ }}></Route>
+            </Routes>
     );
 }
 
