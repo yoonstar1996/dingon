6c330a52485d2f80dbde81daab235aa418d9ed55
CHOIALTH <dnjswns4545@gmail.com>
2022-10-20 16:53:28 +0900

    mainBoard fixed

--- front/src/component/Main.jsx
+++ front/src/component/Main.jsx
@@ -1,55 +1,26 @@
 import React from "react";
 import AssignModal from "./AssignModal";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-  useParams,
-} from "react-router-dom";
-import Sticky from "./Sticky";
-import "../css/Sticky.css";
-import "../css/AssignModal.css";
+import { BrowserRouter, Routes, Route, Link, useParams } from "react-router-dom"
+import Sticky from "./Sticky"
+import "../css/Sticky.css"
+import "../css/AssignModal.css"
 import MainBoards from "./MainBoards";
 function Main() {
-  return (
-    <Routes>
-      <Route path="/" element={<MainBoards />}></Route>
-      <Route
-        path="/post"
-        element={
-          {
-            /*기영*/
-          }
-        }
-      ></Route>
-      <Route
-        path="/postupdate"
-        element={
-          {
-            /*기영*/
-          }
-        }
-      ></Route>
-      <Route
-        path="/gallery/:name"
-        element={
-          {
-            /*경민*/
-          }
-        }
-      ></Route>
-      <Route
-        path="/gallery/made"
-        element={
-          {
-            /*정우*/
-          }
-        }
-      ></Route>
-      {/* <Route path="/" element={<MainBoards />}></Route> */}
-    </Routes>
-  );
+
+
+
+    return (
+        <>
+                <Routes>
+                    <Route path="/" element={<MainBoards />}></Route>
+                    <Route path="/게시글" element={{/*기영*/}}></Route>
+                    <Route path="/게시글수정" element={{/*기영*/}}></Route>
+                    <Route path="/gallery/:name" element={{/*경민*/}}></Route>
+                    <Route path="/gallery/made" element={{/*정우*/}}></Route>
+                    <Route path="/*" element={{/*원준*/}}></Route>
+                </Routes>
+        </>
+    );
 }
 
 export default Main;
