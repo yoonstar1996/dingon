66dcf5921cbf1a88233522a4d3547d9593283fb3
CHOIALTH <dnjswns4545@gmail.com>
2022-10-20 13:19:27 +0900

    mainBoard commit2

--- front/src/component/Main.jsx
+++ front/src/component/Main.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import AssignModal from "./AssignModal";
-import { BrowserRouter, Routes, Route, Link, useParams } from "react-router-dom"
-import Sticky from "./Sticky"
-import "../css/Sticky.css"
-import "../css/AssignModal.css"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
+import Sticky from "./Sticky";
+import "../css/Sticky.css";
+import "../css/AssignModal.css";
 function Main() {
-
-
-
-    return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<div>wefwe</div>}></Route>
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>wefwe</div>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default Main;
