c2aee5cd059ba0b712c9e679b0024a1db246d1e0
jeongwoo <mbh219@naver.com>
2022-10-26 16:32:15 +0900

    개추 버튼

--- front/src/component/Button/Good.jsx
+++ front/src/component/Button/Good.jsx
@@ -1,18 +1,32 @@
 import React from "react"
 import  Button  from "@mui/material/Button"
 import StarBorderIcon from "@mui/icons-material/StarBorder";
-import { width } from "@mui/system";
 
 
 export default function GoodBtn(){
-    return(
-    <>
-    <Button 
-    style={{borderRadius: "200px", width:"50px",paddingLeft:"25px" }}
-    id="GoodBtn"
-    variant="contained" 
-    startIcon={<StarBorderIcon style={{fontSize:"25px"}}></StarBorderIcon>} 
-    onClick={()=>{}}></Button>
-    </>
-    )
+    return (
+      <>
+        <Button
+          style={{
+            borderRadius: "200px",
+            width: "50px",
+            paddingLeft: "25px",
+            backgroundColor: "#4545AC",
+          }}
+          id="GoodBtn"
+          variant="contained"
+          startIcon={
+            <StarBorderIcon
+              style={{ fontSize: "25px" }}
+              id="Icon"
+              variant="contained"
+            ></StarBorderIcon>
+          }
+          onClick={() => {
+            document.getElementById("Icon").style.color = "yellow";
+            document.getElementById("Icon").variant = "contained"
+          }}
+        ></Button>
+      </>
+    );
 }
